63d366134a44930aef4de274081f3d3d9cfe7930
ninoaddict <adrilbless37@gmail.com>
2023-09-29 01:47:00 +0700

    feat: insyallah imginterpolation final

--- src/ImageInterpolation.java
+++ src/ImageInterpolation.java
@@ -39,28 +39,90 @@ public class ImageInterpolation {
         File file = new File(fileName);
         BufferedImage img = ImageIO.read(file);
 
-        Matrix red = new Matrix(img.getHeight(), img.getWidth());
-        Matrix blue = new Matrix(img.getHeight(), img.getWidth());
-        Matrix green = new Matrix(img.getHeight(), img.getWidth());
+        Matrix red = new Matrix(img.getHeight() + 4, img.getWidth() + 4);
+        Matrix blue = new Matrix(img.getHeight() + 4, img.getWidth() + 4);
+        Matrix green = new Matrix(img.getHeight() + 4, img.getWidth() + 4);
         Matrix tempRed = new Matrix(16, 1);
         Matrix tempBlue = new Matrix(16, 1);
         Matrix tempGreen = new Matrix(16, 1);
 
-        Matrix[][] redRes = new Matrix[img.getHeight() - 3][img.getWidth() - 3];
-        Matrix[][] blueRes = new Matrix[img.getHeight() - 3][img.getWidth() - 3];
-        Matrix[][] greenRes = new Matrix[img.getHeight() - 3][img.getWidth() - 3];
+        Matrix[][] redRes = new Matrix[img.getHeight() + 1][img.getWidth() + 1];
+        Matrix[][] blueRes = new Matrix[img.getHeight() + 1][img.getWidth() + 1];
+        Matrix[][] greenRes = new Matrix[img.getHeight() + 1][img.getWidth() + 1];
 
-        for (int i = 0; i < img.getHeight(); i++) {
-            for (int j = 0; j < img.getWidth(); j++) {
-                int pixel = img.getRGB(j, i);
+        for (int i = 2; i < img.getHeight() + 2; i++) {
+            for (int j = 2; j < img.getWidth() + 2; j++) {
+                int pixel = img.getRGB(j - 2, i - 2);
                 Color color = new Color(pixel, true);
                 red.setElmt(i, j, color.getRed());
                 blue.setElmt(i, j, color.getBlue());
                 green.setElmt(i, j, color.getGreen());
             }
         }
-        for (int i = 1; i <= img.getHeight() - 3; i++) {
-            for (int j = 1; j <= img.getWidth() - 3; j++) {
+
+        for (int i = 2; i < img.getHeight() + 2; i++) {
+            red.setElmt(i, 0, red.getElmt(i, 2));
+            green.setElmt(i, 0, green.getElmt(i, 2));
+            blue.setElmt(i, 0, blue.getElmt(i, 2));
+            red.setElmt(i, 1, red.getElmt(i, 2));
+            green.setElmt(i, 1, green.getElmt(i, 2));
+            blue.setElmt(i, 1, blue.getElmt(i, 2));
+            red.setElmt(i, img.getWidth() + 2, red.getElmt(i, img.getWidth() + 1));
+            green.setElmt(i, img.getWidth() + 2, green.getElmt(i, img.getWidth() + 1));
+            blue.setElmt(i, img.getWidth() + 2, blue.getElmt(i, img.getWidth() + 1));
+            red.setElmt(i, img.getWidth() + 3, red.getElmt(i, img.getWidth() + 1));
+            green.setElmt(i, img.getWidth() + 3, green.getElmt(i, img.getWidth() + 1));
+            blue.setElmt(i, img.getWidth() + 3, blue.getElmt(i, img.getWidth() + 1));
+        }
+
+        for (int j = 2; j < img.getWidth() + 2; j++) {
+            red.setElmt(0, j, red.getElmt(2, j));
+            green.setElmt(0, j, green.getElmt(2, j));
+            blue.setElmt(0, j, blue.getElmt(2, j));
+            red.setElmt(1, j, red.getElmt(2, j));
+            green.setElmt(1, j, green.getElmt(2, j));
+            blue.setElmt(1, j, blue.getElmt(2, j));
+            red.setElmt(img.getHeight() + 2, j, red.getElmt(img.getHeight() + 1, j));
+            green.setElmt(img.getHeight() + 2, j, green.getElmt(img.getHeight() + 1, j));
+            blue.setElmt(img.getHeight() + 2, j, blue.getElmt(img.getHeight() + 1, j));
+            red.setElmt(img.getHeight() + 3, j, red.getElmt(img.getHeight() + 1, j));
+            green.setElmt(img.getHeight() + 3, j, green.getElmt(img.getHeight() + 1, j));
+            blue.setElmt(img.getHeight() + 3, j, blue.getElmt(img.getHeight() + 1, j));
+        }
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                red.setElmt(i, j, red.getElmt(2, 2));
+                green.setElmt(i, j, green.getElmt(2, 2));
+                blue.setElmt(i, j, blue.getElmt(2, 2));
+            }
+        }
+
+        for (int i = img.getHeight() + 2; i < img.getHeight() + 4; i++) {
+            for (int j = 0; j < 2; j++) {
+                red.setElmt(i, j, red.getElmt(img.getHeight() + 1, 2));
+                green.setElmt(i, j, green.getElmt(img.getHeight() + 1, 2));
+                blue.setElmt(i, j, blue.getElmt(img.getHeight() + 1, 2));
+            }
+        }
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = img.getWidth() + 2; j < img.getWidth() + 4; j++) {
+                red.setElmt(i, j, red.getElmt(2, img.getWidth() + 1));
+                green.setElmt(i, j, green.getElmt(2, img.getWidth() + 1));
+                blue.setElmt(i, j, blue.getElmt(2, img.getWidth() + 1));
+            }
+        }
+
+        for (int i = img.getHeight() + 2; i < img.getHeight() + 4; i++) {
+            for (int j = img.getWidth() + 2; j < img.getWidth() + 4; j++) {
+                red.setElmt(i, j, red.getElmt(img.getHeight() + 1, img.getWidth() + 1));
+                green.setElmt(i, j, green.getElmt(img.getHeight() + 1, img.getWidth() + 1));
+                blue.setElmt(i, j, blue.getElmt(img.getHeight() + 1, img.getWidth() + 1));
+            }
+        }
+        for (int i = 1; i <= img.getHeight() + 1; i++) {
+            for (int j = 1; j <= img.getWidth() + 1; j++) {
                 int idx = 0;
                 for (int y = j - 1; y < j + 3; y++) {
                     for (int x = i - 1; x < i + 3; x++) {
@@ -84,56 +146,35 @@ public class ImageInterpolation {
 
         for (int i = 0; i < newHeight; i++) {
             for (int j = 0; j < newWidth; j++) {
+
                 double conX = ((double) img.getHeight() / (double) newHeight) * ((double) (2 * i + 1) / (double) 2)
-                        - 0.5;
-                double conY = ((double) img.getWidth() / (double) newWidth) * ((double) (2 * j + 1) / (double) 2) - 0.5;
-                if (1 <= conX && conX <= (double) img.getHeight() - 3 && 1 <= conY
-                        && conY <= (double) img.getWidth() - 3) {
-                    int x, y, rgb = 0;
-                    x = (int) Math.floor(conX);
-                    x -= 1;
-                    conX -= Math.floor(conX);
-                    y = (int) Math.floor(conY);
-                    y -= 1;
-                    conY -= Math.floor(conY);
-                    double r = calcRGB(redRes[x][y], conX, conY);
-                    double g = calcRGB(greenRes[x][y], conX, conY);
-                    double b = calcRGB(blueRes[x][y], conX, conY);
-                    rgb = 0;
-                    if (r >= 0 && r <= 255)
-                        rgb += (int) Math.round(r);
-                    else if (r > 255)
-                        rgb += 255;
-                    rgb <<= 8;
-                    if (g >= 0 && g <= 255)
-                        rgb += (int) Math.round(g);
-                    else if (g > 255) {
-                        rgb += 255;
-                    }
-                    rgb <<= 8;
-                    if (b >= 0 && b <= 255)
-                        rgb += (int) Math.round(b);
-                    else if (b > 255) {
-                        rgb += 255;
-                    }
-                    newImage.setRGB(j, i, rgb);
-                } else {
-                    int x, y, rgb;
-                    if (conX < 0) {
-                        x = 0;
-                    } else {
-                        x = (int) Math.round(conX);
-                    }
-                    if (conY < 0) {
-                        y = 0;
-                    } else {
-                        y = (int) Math.round(conY);
-                    }
-                    rgb = (int) red.getElmt(x, y);
-                    rgb = (rgb << 8) + (int) green.getElmt(x, y);
-                    rgb = (rgb << 8) + (int) blue.getElmt(x, y);
-                    newImage.setRGB(j, i, rgb);
+                        + 1.5;
+                double conY = ((double) img.getWidth() / (double) newWidth) * ((double) (2 * j + 1) / (double) 2) + 1.5;
+                int x = (int) Math.floor(conX) - 1;
+                conX -= Math.floor(conX);
+                int y = (int) Math.floor(conY) - 1;
+                conY -= Math.floor(conY);
+                double r = calcRGB(redRes[x][y], conX, conY);
+                double g = calcRGB(greenRes[x][y], conX, conY);
+                double b = calcRGB(blueRes[x][y], conX, conY);
+                int rgb = 0;
+                if (r >= 0 && r <= 255)
+                    rgb += (int) Math.round(r);
+                else if (r > 255)
+                    rgb += 255;
+                rgb <<= 8;
+                if (g >= 0 && g <= 255)
+                    rgb += (int) Math.round(g);
+                else if (g > 255) {
+                    rgb += 255;
                 }
+                rgb <<= 8;
+                if (b >= 0 && b <= 255)
+                    rgb += (int) Math.round(b);
+                else if (b > 255) {
+                    rgb += 255;
+                }
+                newImage.setRGB(j, i, rgb);
             }
         }
         try {
